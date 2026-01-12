1fb89dfa446e9055620d57761aecf7892911b8bc
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-01-12 13:36:44 +0400

    Square DONE;

---
+++ AbxtractGeometry/Main.cpp
@@ -0,0 +1,92 @@
+#pragma warning (disable:4326)
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+enum Color
+{
+	Red    = 0x000000FF,
+	Green  = 0x0000FF00,
+	Blue   = 0x00FF0000,
+	Yellow = 0x0000FFFF,
+	Purple = 0x00800080
+};
+
+#define tab "\t"
+#define delimiter "\n----------------------------------------------------------------------\n"
+
+class Shape
+{
+	Color color;
+public:
+	Shape(Color color) :color(color) {}
+	virtual ~Shape() {}
+	virtual double get_area() const = 0;
+	virtual double get_perimeter() const = 0;
+	virtual void draw() const = 0;
+	virtual void info()const
+	{
+		draw();
+		cout << "Площадь фигуры: " << get_area() << endl;
+		cout << "Периметр фигуры: " << get_perimeter() << endl;
+	}
+};
+
+class Square :public Shape
+{
+	double side;
+public:
+	Square(double side, Color color) :Shape(color)
+	{
+		this->side = side;
+	}
+	~Square() {}
+	void set_side(double side)
+	{
+		this->side = side;
+	}
+	double get_side()const
+	{
+		return side;
+	}
+	double get_area() const override
+	{
+		return side * side;
+	}
+	double get_perimeter() const override
+	{
+		return side * 4;
+	}
+	void draw() const override
+	{
+		for (int i = 0; i < side; i++)
+		{
+			for (int i = 0; i < side; i++)
+			{
+				cout << "* ";
+			}
+			cout << endl;
+		}
+	}
+	void info() const override
+	{
+		cout << typeid(*this).name() << endl;
+		cout << "Длина Стороны: " << get_side() << endl;
+		Shape::info();
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	//Shape shape = Color::Red;
+	Square square(5, Color::Red);
+	/*cout << "Сторона квадрата: " << square.get_side() << endl;
+	cout << "Площадь фигуры: " << square.get_area() << endl;
+	cout << "Периметр фигуры: " << square.get_perimeter() << endl;
+	square.draw();*/
+	square.info();
+}
