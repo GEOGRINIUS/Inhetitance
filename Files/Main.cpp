62fb4dabc839957b21b56127b6b3d5f0a4cd812a
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-01-28 11:53:18 +0400

    Angled Radius ODNE;

--- Files/Main.cpp
+++ Files/Main.cpp
@@ -1,21 +1,32 @@
 #pragma warning (disable:4326)
 #include<iostream>
 #include<fstream>	//0) подключение файловые потокий
-
 using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
 
+//#define WRITE_TO_FILE
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	//1) Создаем поток:
+
+#ifdef WRITE_TO_FILE
+
 	std::ofstream fout;
-	//2) Открываем поток:
-	fout.open("file.txt");
-	//3) Записываем поток:
+	fout.open("file.txt", std::ios_base::app);
 	fout << "Hello world!" << endl;
-	//4) Закрываем поток:
 	fout.close();
+
+	system("notepad file.txt");	//Функция system() позволяет выполнить любую команду из командной строки Windows (cmd.exe)
+#endif // WRITE_TO_FILE
+
+	cout << "Your name please: ";
+	const int SIZE = 256;
+	char last_name[SIZE] = {};
+	char first_name[SIZE] = {};
+	cin >> first_name >> last_name;
+	cout << first_name << "\t" << last_name << endl;
+
 }
